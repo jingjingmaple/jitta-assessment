1fb52138ca2a54ea88e4003f52a20087a7ec02e1
Sorachat Patanasombat <sorachat.p@haupcar.com>
2024-12-18 16:54:15 +0700

    Create project structure, Install flutter_bloc and graphql library

---
+++ lib/views/landing_page/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({ Key? key }) : super(key: key);
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Home')
+    );
+  }
+}
